233c7a1775c2d16762818225eb2a8ffb26a1c5e1
Greg <greg.rodriguez@outlook.com>
2022-06-07 09:10:01 -0600

    loader function and loader variables

--- infinite_scroll/scripts.js
+++ infinite_scroll/scripts.js
@@ -1,12 +1,24 @@
 const imageContainer = document.getElementById('imageContainer');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = '8Avx-jRghqyJtkJ1sJ7bt6gvAIcjkKPyXzQfD1UOw2Q';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 // Helper function to set attributes on DOM elements
 function setAttributes(element, attributes) {
@@ -17,6 +29,8 @@ function setAttributes(element, attributes) {
 
 // Create elements to the DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
         const item = document.createElement('a');
@@ -36,6 +50,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        // Event listener to check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         // Put <img> inside <a>, put both inside container element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -54,8 +70,8 @@ async function getPhotosFromApi() {
 
 // Check if scrolling is near bottom of page
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
-        console.log("load more");
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
     }
 });
 
