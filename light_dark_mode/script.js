5124dbb2bb3406a5b6a047103337e8a8df801643
Greg <greg.rodriguez@outlook.com>
2022-06-12 16:28:01 -0600

    added comment

--- light_dark_mode/script.js
+++ light_dark_mode/script.js
@@ -6,6 +6,16 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Using ternary operators
+// function toggleDarkLightMode(isDark) {
+//     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+//     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+//     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+//     isDark ? toggleIcon.children[1].classList.replace('fa-sun','fa-moon'):
+//         toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+//     isDark ? imageMode('dark') : imageMode('light');
+// }
+
 function imageMode(color) {
     image1.src = `img/undraw_proud_coder_${color}.svg`;
     image2.src = `img/undraw_feeling_proud_${color}.svg`;
