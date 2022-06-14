bdda44ee37610914f677fb4fc5d618c61ce41764
Greg <greg.rodriguez@outlook.com>
2022-06-14 15:55:08 -0600

    add event listeners, nav toggle function

--- navigation-nation/script.js
+++ navigation-nation/script.js
@@ -6,3 +6,14 @@ const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 
+function toggleNav() {
+    menuBars.classList.toggle('change');
+}
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
