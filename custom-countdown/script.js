04ceeaead95e507d9cdd729b3830b96f3e6c7b3b
Greg <greg.rodriguez@outlook.com>
2022-06-17 09:54:00 -0600

    update countdown function and event listener

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,6 +2,18 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set minimum date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+}
+
+// Event listener
+countdownForm.addEventListener('submit', updateCountdown);
