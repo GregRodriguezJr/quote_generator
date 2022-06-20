3bebfc7722fa62adae7eac9bb653bc69fbe15d2b
Greg <greg.rodriguez@outlook.com>
2022-06-20 08:53:42 -0600

    added form validation

--- book_keeper/script.js
+++ book_keeper/script.js
@@ -12,13 +12,42 @@ function showModal() {
     websiteNameEl.focus();
 }
 
-function storeBookmark(e) {
-    e.preventDefault();
-    const nameValue = websiteNameEl.value;
-    let urlValue = websiteUrlEl.value;
-}
-
+// Modal event listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e)=> e.target === modal ? modal.classList.remove('show-modal') : false);
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+// Validate form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields.');
+        return false;
+    }
+    if(!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;    
+    }
+    // Valid
+    return true;
+}
+
+// Handle data from form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    };
+}
+
+
+
+
